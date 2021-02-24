262decd287340909535b62ed979f209afde2936f
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-23 22:43:01 -0800

    populated some seeds for the class migration

--- data/migrations/20210223221942_fitness.js
+++ data/migrations/20210223221942_fitness.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("class", (tbl) => {
+    tbl.increments("id");
+    tbl.text("class_name", 128).notNullable();
+    tbl.text("class_type").notNullable();
+    tbl.text("intensity");
+    tbl.text("duration");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("class");
 };
--- data/seeds/01-class.js
+++ data/seeds/01-class.js
@@ -1,13 +1,38 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex("class")
+    .del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("class").insert([
+        {
+          id: 1,
+          class_name: "Hot yoga with Kato",
+          class_type: "yoga",
+          intensity: "Intermediate",
+          duration: "45 mins",
+        },
+        {
+          id: 2,
+          class_name: "HIIT with Danielle",
+          class_type: "High intensity interval training",
+          intensity: "Hard",
+          duration: "45 mins",
+        },
+        {
+          id: 3,
+          class_name: "Zumba with Kyle",
+          class_type: "zumba",
+          intensity: "intermediate",
+          duration: "45 mins",
+        },
+        {
+          id: 4,
+          class_name: "Pole Dancing with Austin",
+          class_type: "pole",
+          intensity: "hard",
+          duration: "45 mins",
+        },
       ]);
     });
 };
