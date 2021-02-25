5884c3016e5e3162e5d21b99a8b883bfe8dfaf23
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-24 19:48:21 -0800

    created push to add new class

--- api/classes/classes-model.js
+++ api/classes/classes-model.js
@@ -3,6 +3,7 @@ const db = require("../../data/db-config");
 module.exports = {
   find,
   findById,
+  add,
 };
 
 function find() {
@@ -11,4 +12,12 @@ function find() {
 
 function findById(id) {
   return db("classes").where({ id }).first();
+}
+
+function add(newClass) {
+  return db("classes")
+    .insert(newClass)
+    .then((ids) => {
+      return findById(ids[0]);
+    });
 }
--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -15,12 +15,31 @@ router.get("/:id", async (req, res) => {
   const { id } = req.params;
   const classById = await Classes.findById(id);
   if (!classById) {
-    res.status(401).json({ message: "could not find class byt the id" });
+    res.status(401).json({ message: "could not find class by the id" });
   } else {
     try {
       res.status(200).json(classById);
     } catch (err) {
       res.status(500).json({ message: "server error" });
+    }
+  }
+});
+
+router.post("/", async (req, res) => {
+  const classBody = req.body;
+
+  if (!classBody.class_name || !classBody.class_type || !classBody.date) {
+    res
+      .status(404)
+      .json({ message: "A class name, type and date are required" });
+  } else {
+    try {
+      const newClass = await Classes.add(classBody);
+      res.status(201).json(newClass);
+    } catch (err) {
+      res
+        .status(500)
+        .json({ message: "Could not create your class at this moment" });
     }
   }
 });
