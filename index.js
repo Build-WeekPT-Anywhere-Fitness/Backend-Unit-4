0b2d3ea1be416d777fd1a9d0972ed37b9e94507b
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-24 18:51:04 -0800

    testing if it works

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const server = require("./api/server");
 
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "it's working!" });
+});
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
