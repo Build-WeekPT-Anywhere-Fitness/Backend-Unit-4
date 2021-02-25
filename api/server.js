44f98b5e08b1a3c33bd3bf82996e747b5a1384da
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-24 18:58:32 -0800

    added helmet and cors

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const ClassesRouter = require("./classes/classes-router");
 
 const server = express();
 
-server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.server.use(express.json());
 server.use("/api/classes", ClassesRouter);
 
 module.exports = server;
