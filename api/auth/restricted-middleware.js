e89a6658fa5b2c9e377724a0c6302c6ae4968508
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-26 20:31:35 -0800

    more token

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -3,8 +3,7 @@ const jwt = require("jsonwebtoken");
 const secret = require("../config/secrets");
 
 module.exports = (req, res, next) => {
-  const token =
-    req.headers.authorization && req.headers.authorization.split(" ")[1];
+  const token = req.headers?.authorization?.split(" ")[1];
 
   if (token) {
     jwt.verify(token, secret, (err, decodedToken) => {
